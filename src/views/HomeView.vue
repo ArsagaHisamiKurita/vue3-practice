feb04833fb7da08a6f216c6efbd809a35f5d1737
Hisami Kurita <hisami.kurita@arsaga.jp>
2024-03-07 10:53:38 +0900

    feat: vue3環境

---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <main>
+    <button @click="increment">増える</button>
+    {{ count }}
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { ref } from 'vue'
+import TheWelcome from '../components/TheWelcome.vue'
+
+  export default defineComponent({
+    components: {
+      TheWelcome
+    },
+    setup() {
+      const count = ref(0)
+
+      const increment = () => {
+        count.value++
+      }
+
+      return {
+        count,
+        increment
+      }
+    }
+  })
+</script>
